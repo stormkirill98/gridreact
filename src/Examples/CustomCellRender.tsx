ba541b6a849d7e968f80c9942d6c10de810c8a69
ka.panihin <ka.panihin@tensor.ru>
2022-12-08 13:21:01 +0600

    toggle selection with custom cell render

--- src/Examples/CustomCellRender.tsx
+++ src/Examples/CustomCellRender.tsx
@@ -3,7 +3,7 @@ import {Column, Grid, ICellRenderParams, TRowEventHandler} from '../Controls/gri
 import TaskRender from './CellRenders/TaskRender';
 
 import {generateData} from './Data';
-import {useMarker} from './utils';
+import {useMarker, useSelection} from './utils';
 const TABLE_DATA = generateData(20);
 
 function BoldText(params: ICellRenderParams): React.ReactElement {
@@ -27,13 +27,17 @@ function CustomText(params: ICellRenderParams): string {
 
 export default function CustomCellRender(): React.ReactElement {
    const markerProps = useMarker();
+   const {selectionVisibility, toggleButton} = useSelection();
    return (
-      <Grid data={TABLE_DATA} keyProperty='key' {...markerProps}>
-         <Column displayProperties={['a']} width='1fr' render={React.memo(BoldText)}/>
-         <Column displayProperties={['b']} width='1fr' render={React.memo(BorderCell)}/>
-         <Column displayProperties={['c']} width='1fr' render={CustomText}/>
-         <Column displayProperties={['d']} width='400px' render={React.memo(TaskRender)}/>
-         <Column displayProperties={['e']} width='1fr' />
-      </Grid>
+      <div>
+         {toggleButton}
+         <Grid data={TABLE_DATA} keyProperty='key' {...markerProps} selectionVisibility={selectionVisibility}>
+            <Column displayProperties={['a']} width='1fr' render={React.memo(BoldText)}/>
+            <Column displayProperties={['b']} width='1fr' render={React.memo(BorderCell)}/>
+            <Column displayProperties={['c']} width='1fr' render={CustomText}/>
+            <Column displayProperties={['d']} width='400px' render={React.memo(TaskRender)}/>
+            <Column displayProperties={['e']} width='1fr' />
+         </Grid>
+      </div>
    );
 }
