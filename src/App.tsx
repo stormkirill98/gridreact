976a8091d9b8a656620fd07c7aac7ec47fa09b2c
Кирилл Панихин <stormkirill98@gmail.com>
2023-01-24 10:38:14 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 function Render(props: any) {
   const [state, setState] = React.useState(0);
-
   return (
     <div>
       {props.item + ", state=" + state}
@@ -17,8 +16,13 @@ function Render(props: any) {
 
 const RenderMemo = React.memo(Render);
 
-function Wrapper(props: { render: React.ReactElement; item: string }) {
-  return <div>{React.cloneElement(props.render, { item: props.item })}</div>;
+function Wrapper(props: { render: React.FunctionComponent; item: string }) {
+  return (
+    <div>
+      {/* @ts-ignore */}
+      <props.render item={props.item} />
+    </div>
+  );
 }
 
 export default function App() {
@@ -27,17 +31,17 @@ export default function App() {
   return (
     <div className="App">
       {/* @ts-ignore */}
-      {/* <Wrapper item="var 1" render={(item) => <Render item={item} />} /> */}
+      <Wrapper item="var 1" render={(props) => <Render item={props.item} />} />
       {/* @ts-ignore */}
-      {/* <Wrapper item="var 2" render={(item) => <div>{item}</div>} /> */}
+      <Wrapper item="var 2" render={(props) => <div>{props.item}</div>} />
       {/* @ts-ignore */}
-      {/* <Wrapper item="var 3" render={Render} /> */}
+      <Wrapper item="var 3" render={Render} />
       {/* @ts-ignore */}
       <Wrapper item="var 4" render={RenderMemo} />
       {/* @ts-ignore */}
-      <Wrapper item="var 5" render={<Render item={"var5"} />} />
+      {/* <Wrapper item="var 5" render={<Render item={"var5"} />} /> */}
       {/* @ts-ignore */}
-      {/* <Wrapper item="var 6" render={(item) => null} /> */}
+      <Wrapper item="var 6" render={(item) => null} />
 
       <br />
       <br />
